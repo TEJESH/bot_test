2e58669423edd51a4b58cc37aac65faa459ef5ed
Tejesh Agrawal <tejeshagrawal@gmail.com>
2016-12-06 03:27:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,62 +1,49 @@
+#!/usr/bin/env python
+
 # Copyright (C) 2016 Javier Ayres
 #
 # This file is part of python-telegram-bot-openshift.
-#
+# 
 # python-telegram-bot-openshift is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-#
+# 
 # python-telegram-bot-openshift is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-#
+# 
 # You should have received a copy of the GNU General Public License
 # along with python-telegram-bot-openshift.  If not, see <http://www.gnu.org/licenses/>.
 
-import logging
-from queue import Queue
-from threading import Thread
-from telegram import Bot
-from telegram.ext import Dispatcher, MessageHandler, Updater
-
-TOKEN = '292106014:AAEdLmqqhYHhDncqidNtFSNx9Mj7Fil50_8'
+import os
+import bot
+from flask import Flask, request
+from telegram import Update
 
 
-def example_handler(bot, update):
-    # Remove this handler
-    bot.send_message(
-        update.message.chat_id,
-        text='Hello from openshift'
-    )
-
-# Write your handlers here
+application = Flask(__name__, instance_path=os.environ['OPENSHIFT_REPO_DIR'])
+update_queue, bot_instance = bot.setup(webhook_url='https://{}/{}'.format(
+    os.environ['OPENSHIFT_GEAR_DNS'],
+    bot.TOKEN
+))
 
 
-def setup(webhook_url=None):
-    """If webhook_url is not passed, run with long-polling."""
-    logging.basicConfig(level=logging.WARNING)
-    if webhook_url:
-        bot = Bot(TOKEN)
-        update_queue = Queue()
-        dp = Dispatcher(bot, update_queue)
-    else:
-        updater = Updater(TOKEN)
-        bot = updater.bot
-        dp = updater.dispatcher
-    dp.add_handler(MessageHandler([], example_handler))  # Remove this line
-    # Add your handlers here
-    if webhook_url:
-        bot.set_webhook(webhook_url=webhook_url)
-        thread = Thread(target=dp.start, name='dispatcher')
-        thread.start()
-        return update_queue, bot
-    else:
-        bot.set_webhook()  # Delete webhook
-        updater.start_polling()
-        updater.idle()
+@application.route('/')
+def not_found():
+    """Server won't respond in OpenShift if we don't handle the root path."""
+    return ''
+
+
+@application.route('/' + bot.TOKEN, methods=['GET', 'POST'])
+def webhook():
+    if request.json:
+        update_queue.put(Update.de_json(request.json, bot_instance))
+    return ''
 
 
 if __name__ == '__main__':
-    setup()
+    ip = os.environ['OPENSHIFT_PYTHON_IP']
+    port = int(os.environ['OPENSHIFT_PYTHON_PORT'])
+    application.run(host=ip, port=port)
